08a8daef00ae00f0482dfe595c27a5ae1a34d839
Jake Turner <jake@unity3d.com>
2020-03-28 21:29:40 +0000

    Day2 Part1 & Part2 done

--- AoC2016/Day02/Day02.cs
+++ AoC2016/Day02/Day02.cs
@@ -10,7 +10,8 @@ You arrive at Easter Bunny Headquarters under cover of darkness. However, you le
 
 The document goes on to explain that each button to be pressed can be found by starting on the previous button and moving to adjacent buttons on the keypad: U moves up, D moves down, L moves left, and R moves right. Each line of instructions corresponds to one button, starting at the previous button (or, for the first line, the "5" button); press whatever button you're on at the end of each line. If a move doesn't lead to a button, ignore it.
 
-You can't hold it much longer, so you decide to figure out the code as you walk to the bathroom. You picture a keypad like this:
+You can't hold it much longer, so you decide to figure out the code as you walk to the bathroom. 
+You picture a keypad like this:
 
 1 2 3
 4 5 6
@@ -29,6 +30,27 @@ So, in this example, the bathroom code is 1985.
 
 Your puzzle input is the instructions from the document you found at the front desk. What is the bathroom code?
 
+95549
+
+--- Part Two ---
+
+You finally arrive at the bathroom (it's a several minute walk from the lobby so visitors can behold the many fancy conference rooms and water coolers on this floor) and go to punch in the code. Much to your bladder's dismay, the keypad is not at all like you imagined it. Instead, you are confronted with the result of hundreds of man-hours of bathroom-keypad-design meetings:
+
+    1
+  2 3 4
+5 6 7 8 9
+  A B C
+    D
+
+You still start at "5" and stop when you're at an edge, but given the same instructions as above, the outcome is very different:
+
+You start at "5" and don't move at all (up and left are both edges), ending at 5.
+Continuing from "5", you move right twice and down three times (through "6", "7", "B", "D", "D"), ending at D.
+Then, from "D", you move five more times (through "D", "B", "C", "C", "B"), ending at B.
+Finally, after five more moves, you end at 3.
+So, given the actual keypad layout, the code would be 5DB3.
+
+Using the same instructions in your puzzle input, what is the correct bathroom code?
 */
 
 namespace Day02
@@ -40,9 +62,9 @@ namespace Day02
             var lines = AoC.Program.ReadLines(inputFile);
             if (part1)
             {
-                long result1 = -666;
+                long result1 = KeyCode(lines);
                 Console.WriteLine($"Day02 : Result1 {result1}");
-                long expected = 280;
+                long expected = 95549;
                 if (result1 != expected)
                 {
                     throw new InvalidProgramException($"Part1 is broken {result1} != {expected}");
@@ -50,14 +72,126 @@ namespace Day02
             }
             else
             {
-                long result2 = -123;
+                var result2 = KeyCode5x5(lines);
                 Console.WriteLine($"Day02 : Result2 {result2}");
-                long expected = 1797;
+                var expected = "D87AD";
                 if (result2 != expected)
                 {
                     throw new InvalidProgramException($"Part2 is broken {result2} != {expected}");
                 }
             }
+        }
+
+        public static int KeyCode(string[] codes)
+        {
+            var x = 0;
+            var y = 0;
+            var keyCode = 0;
+            foreach (var code in codes)
+            {
+                foreach (var m in code)
+                {
+                    var dx = 0;
+                    var dy = 0;
+                    if (m == 'U')
+                    {
+                        dy = -1;
+                    }
+                    else if (m == 'D')
+                    {
+                        dy = +1;
+                    }
+                    else if (m == 'L')
+                    {
+                        dx = -1;
+                    }
+                    else if (m == 'R')
+                    {
+                        dx = +1;
+                    }
+                    x += dx;
+                    y += dy;
+                    x = Math.Clamp(x, -1, +1);
+                    y = Math.Clamp(y, -1, +1);
+                }
+                var key = 1 + (x + 1) + (y + 1) * 3;
+                keyCode *= 10;
+                keyCode += key;
+            }
+            return keyCode;
+        }
+
+        public static string KeyCode5x5(string[] codes)
+        {
+            int x;
+            int y;
+            var charCode = new char[5, 5];
+            for (y = 0; y < 5; ++y)
+            {
+                for (x = 0; x < 5; ++x)
+                {
+                    charCode[x, y] = '*';
+                }
+            }
+            /*
+                1
+              2 3 4
+            5 6 7 8 9
+              A B C
+                D
+            */
+            charCode[2, 0] = '1';
+            charCode[1, 1] = '2';
+            charCode[2, 1] = '3';
+            charCode[3, 1] = '4';
+            charCode[0, 2] = '5';
+            charCode[1, 2] = '6';
+            charCode[2, 2] = '7';
+            charCode[3, 2] = '8';
+            charCode[4, 2] = '9';
+            charCode[1, 3] = 'A';
+            charCode[2, 3] = 'B';
+            charCode[3, 3] = 'C';
+            charCode[2, 4] = 'D';
+            x = 0;
+            y = 2;
+            string keyCode = "";
+            foreach (var code in codes)
+            {
+                foreach (var m in code)
+                {
+                    var dx = 0;
+                    var dy = 0;
+                    if (m == 'U')
+                    {
+                        dy = -1;
+                    }
+                    else if (m == 'D')
+                    {
+                        dy = +1;
+                    }
+                    else if (m == 'L')
+                    {
+                        dx = -1;
+                    }
+                    else if (m == 'R')
+                    {
+                        dx = +1;
+                    }
+                    var newX = x + dx;
+                    var newY = y + dy;
+                    newX = Math.Clamp(newX, 0, 4);
+                    newY = Math.Clamp(newY, 0, 4);
+                    var c = charCode[newX, newY];
+                    if (c != '*')
+                    {
+                        x = newX;
+                        y = newY;
+                    }
+                }
+                keyCode += charCode[x, y];
+            }
+            return keyCode;
         }
 
         public static void Run()
--- AoC2016/Day02/Day02Tests.cs
+++ AoC2016/Day02/Day02Tests.cs
@@ -1,15 +1,50 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace Day02
 {
     [TestFixture]
     public class Tests
     {
+        static string[] testCodes = new string[] {
+"ULL",
+"RRDDD",
+"LURDL",
+"UUUUD"
+        };
+
         [Test]
-        [TestCase("", -3)]
-        public void Day02(string directions, int expected)
+        [TestCase("ULL", 1)]
+        [TestCase("RRDDD", 9)]
+        [TestCase("LURDL", 4)]
+        [TestCase("UUUUD", 5)]
+        public void KeyCode(string code, int expectedKey)
         {
-            Assert.Fail();
+            Assert.That(Program.KeyCode(new string[] { code }), Is.EqualTo(expectedKey));
+        }
+
+        public static IEnumerable<TestCaseData> TestCodeCases = new[]
+        {
+            new TestCaseData(testCodes, 1985).SetName("TestCode 1985")
+        };
+
+        [Test]
+        [TestCaseSource("TestCodeCases")]
+        public void KeyCode(string[] codes, int expectedCode)
+        {
+            Assert.That(Program.KeyCode(codes), Is.EqualTo(expectedCode));
+        }
+
+        public static IEnumerable<TestCaseData> TestCode5x5Cases = new[]
+        {
+            new TestCaseData(testCodes, "5DB3").SetName("TestCode5x5 5DB3")
+        };
+
+        [Test]
+        [TestCaseSource("TestCode5x5Cases")]
+        public void KeyCode5x5(string[] codes, string expectedCode)
+        {
+            Assert.That(Program.KeyCode5x5(codes), Is.EqualTo(expectedCode));
         }
     }
 }
