ef1ed68cff4dc21e3a10ff0ed017a873e9dda29e
Jake Turner <jake@unity3d.com>
2020-04-01 12:14:12 +0100

    First commit for Day08

--- AoC2016/Day08/Day08Tests.cs
+++ AoC2016/Day08/Day08Tests.cs
@@ -5,11 +5,40 @@ namespace Day08
     [TestFixture]
     public class Tests
     {
-        [Test]
-        [TestCase("", -3)]
-        public void Day08(string directions, int expected)
+        public static string[] rect3x2result = new string[]
         {
-            Assert.Fail();
+"###....",
+"###....",
+"......."
+        };
+
+        public static string[] rotateColumnx1by1 = new string[]
+        {
+"#.#....",
+"###....",
+".#....."
+        };
+
+        public static string[] rotateRowy0by4 = new string[]
+        {
+"....#.#",
+"###....",
+".#....."
+        };
+
+        public static TestCaseData[] TestCommandTests = new TestCaseData[]
+        {
+            new TestCaseData("rect 3x2", rect3x2result).SetName("rect 3x2"),
+            new TestCaseData("rotate column x=1 by 1", rotateColumnx1by1).SetName("rotate column x=1 by 1"),
+            new TestCaseData("rotate row y=0 by 4", rotateRowy0by4).SetName("rotate row y=0 by 4")
+        };
+
+        [Test]
+        [TestCaseSource("TestCommandTests")]
+        public void CommandTests(string command, string[] expectedResult)
+        {
+            Program.ProcessCommand(command);
+            Assert.That(Program.GetGrid(), Is.EqualTo(expectedResult));
         }
     }
 }
