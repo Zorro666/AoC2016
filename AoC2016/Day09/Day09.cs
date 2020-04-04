37590b7f68aac3a98c28e1bac6ae79b1175d80e9
Jake Turner <jake@unity3d.com>
2020-04-04 15:08:33 +0100

    Day 9 Part1 done

--- AoC2016/Day09/Day09.cs
+++ AoC2016/Day09/Day09.cs
@@ -28,7 +28,24 @@ It has a decompressed length of 6.
 X(8x2)(3x3)ABCY becomes X(3x3)ABC(3x3)ABCY (for a decompressed length of 18), because the decompressed data from the (8x2) marker (the (3x3)ABC) is skipped and not processed further.
 
 What is the decompressed length of the file (your puzzle input)? Don't count whitespace.
+    
+Your puzzle answer was 152851.
 
+--- Part Two ---
+
+Apparently, the file actually uses version two of the format.
+
+In version two, the only difference is that markers within decompressed data are decompressed. This, the documentation explains, provides much more substantial compression capabilities, allowing many-gigabyte files to be stored in only a few kilobytes.
+
+For example:
+
+(3x3)XYZ still becomes XYZXYZXYZ, as the decompressed section contains no markers.
+X(8x2)(3x3)ABCY becomes XABCABCABCABCABCABCY, because the decompressed data from the (8x2) marker is then further decompressed, thus triggering the (3x3) marker twice for a total of six ABC sequences.
+(27x12)(20x12)(13x14)(7x10)(1x12)A decompresses into a string of A repeated 241920 times.
+(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN becomes 445 characters long.
+Unfortunately, the computer you brought probably doesn't have enough memory to actually decompress the file; you'll have to come up with another way to get its decompressed length.
+
+What is the decompressed length of the file using this improved format?
 */
 
 namespace Day09
@@ -40,9 +57,9 @@ namespace Day09
             var lines = AoC.Program.ReadLines(inputFile);
             if (part1)
             {
-                long result1 = -666;
+                long result1 = TotalDecompressedLength(lines);
                 Console.WriteLine($"Day09 : Result1 {result1}");
-                long expected = 280;
+                long expected = 152851;
                 if (result1 != expected)
                 {
                     throw new InvalidProgramException($"Part1 is broken {result1} != {expected}");
@@ -58,6 +75,108 @@ namespace Day09
                     throw new InvalidProgramException($"Part2 is broken {result2} != {expected}");
                 }
             }
+        }
+
+        public static string Decompress(string compressed)
+        {
+            var decompressed = "";
+            var foundLeft = false;
+            var foundRight = true;
+            var lookForLeft = true;
+            var lookForRight = false;
+            var repeatString = "";
+            var repeatCharCount = 0;
+            var targetRepeatCharCount = 0;
+            var repeatCount = 0;
+            var targetRepeatCount = 0;
+            foreach (var c in compressed)
+            {
+                if (char.IsWhiteSpace(c))
+                {
+                    continue;
+                }
+                if (targetRepeatCharCount > 0)
+                {
+                    if (repeatCharCount != targetRepeatCharCount)
+                    {
+                        repeatString += c;
+                        ++repeatCharCount;
+                    }
+                    if (repeatCharCount == targetRepeatCharCount)
+                    {
+                        if (repeatCount != targetRepeatCount)
+                        {
+                            for (repeatCount = 0; repeatCount < targetRepeatCount; ++repeatCount)
+                            {
+                                decompressed += repeatString;
+                            }
+                            repeatCount = targetRepeatCount;
+                        }
+                        targetRepeatCharCount = 0;
+                        targetRepeatCount = 0;
+                        repeatCharCount = 0;
+                        repeatCount = 0;
+                    }
+                }
+                else if (lookForLeft && foundRight)
+                {
+                    if (c == '(')
+                    {
+                        lookForLeft = false;
+                        lookForRight = true;
+                        foundLeft = true;
+                        foundRight = false;
+                        repeatString = "";
+                    }
+                    else
+                    {
+                        decompressed += c;
+                    }
+                }
+                else if (lookForRight && foundLeft)
+                {
+                    if (c == ')')
+                    {
+                        lookForLeft = true;
+                        lookForRight = false;
+                        foundLeft = false;
+                        foundRight = true;
+                        repeatCount = 0;
+                        repeatCharCount = 0;
+                        var tokens = repeatString.Split('x');
+                        if (tokens.Length != 2)
+                        {
+                            throw new InvalidProgramException($"Invalid repeatString '{repeatString}' got {tokens.Length} expected 2");
+                        }
+                        targetRepeatCharCount = int.Parse(tokens[0]);
+                        targetRepeatCount = int.Parse(tokens[1]);
+                        repeatString = "";
+                    }
+                    else
+                    {
+                        repeatString += c;
+                    }
+                }
+                else
+                {
+                    throw new InvalidProgramException($"Unknown state when decompresssing");
+                }
+            }
+            DecompressedLength = decompressed.Length;
+            return decompressed;
+        }
+
+        public static int DecompressedLength { get; private set; }
+
+        static int TotalDecompressedLength(string[] lines)
+        {
+            var total = 0;
+            foreach (var line in lines)
+            {
+                var decompressed = Decompress(line);
+                total += DecompressedLength;
+            }
+            return total;
         }
 
         public static void Run()
--- AoC2016/Day09/Day09Tests.cs
+++ AoC2016/Day09/Day09Tests.cs
@@ -6,10 +6,28 @@ namespace Day09
     public class Tests
     {
         [Test]
-        [TestCase("", -3)]
-        public void Day09(string directions, int expected)
+        [TestCase("ADVENT", "ADVENT")]
+        [TestCase("A(1x5)BC", "ABBBBBC")]
+        [TestCase("(3x3)XYZ", "XYZXYZXYZ")]
+        [TestCase("A(2x2)BCD(2x2)EFG", "ABCBCDEFEFG")]
+        [TestCase("(6x1)(1x3)A", "(1x3)A")]
+        [TestCase("X(8x2)(3x3)ABCY", "X(3x3)ABC(3x3)ABCY")]
+        public void Decompressed(string compressed, string expectedString)
         {
-            Assert.Fail();
+            Assert.That(Program.Decompress(compressed), Is.EqualTo(expectedString));
+        }
+
+        [Test]
+        [TestCase("ADVENT", 6)]
+        [TestCase("A(1x5)BC", 7)]
+        [TestCase("(3x3)XYZ", 9)]
+        [TestCase("A(2x2)BCD(2x2)EFG", 11)]
+        [TestCase("(6x1)(1x3)A", 6)]
+        [TestCase("X(8x2)(3x3)ABCY", 18)]
+        public void DecompressedLength(string compressed, int expectedLength)
+        {
+            _ = Program.Decompress(compressed);
+            Assert.That(Program.DecompressedLength, Is.EqualTo(expectedLength));
         }
     }
 }
