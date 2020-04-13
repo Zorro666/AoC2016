486579e677443e20566b569843d623ecd5a84e75
Jake Turner <jake@unity3d.com>
2020-04-13 13:57:59 +0100

    Day12 start

--- AoC2016/Day12/Day12Tests.cs
+++ AoC2016/Day12/Day12Tests.cs
@@ -5,11 +5,25 @@ namespace Day12
     [TestFixture]
     public class Tests
     {
+        public static string[] testProgram = new string[] {
+"cpy 41 a",
+"inc a",
+"inc a",
+"dec a",
+"jnz a 2",
+"dec a"
+        };
+
+        public static TestCaseData[] TestProgramCases = new TestCaseData[] {
+            new TestCaseData(testProgram, 42).SetName("TestProgram a=42")
+        };
+
         [Test]
-        [TestCase("", -3)]
-        public void Day12(string directions, int expected)
+        [TestCaseSource("TestProgramCases")]
+        public void TestCode(string[] code, int expectedA)
         {
-            Assert.Fail();
+            Program.Parse(code);
+            Assert.That(Program.A, Is.EqualTo(expectedA));
         }
     }
 }
