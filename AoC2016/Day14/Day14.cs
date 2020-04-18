7c052daf0e64c4c7d840e3ed4bc40058fa80319d
Jake Turner <jake@unity3d.com>
2020-04-18 11:52:54 +0100

    Day14 Part1 & 2 done

--- AoC2016/Day14/Day14.cs
+++ AoC2016/Day14/Day14.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
 
 /*
 
@@ -31,6 +34,37 @@ Given the actual salt in your puzzle input, what index produces your 64th one-ti
 
 Your puzzle input is zpqevtbw.
 
+
+Your puzzle answer was 16106.
+
+--- Part Two ---
+
+Of course, in order to make this process even more secure, you've also implemented key stretching.
+
+Key stretching forces attackers to spend more time generating hashes. 
+Unfortunately, it forces everyone else to spend more time, too.
+
+To implement key stretching, whenever you generate a hash, before you use it, you first find the MD5 hash of that hash, then the MD5 hash of that hash, and so on, a total of 2016 additional hashings. 
+Always use lowercase hexadecimal representations of hashes.
+
+For example, to find the stretched hash for index 0 and salt abc:
+
+Find the MD5 hash of abc0: 577571be4de9dcce85a041ba0410f29f.
+Then, find the MD5 hash of that hash: eec80a0c92dc8a0777c619d9bb51e910.
+Then, find the MD5 hash of that hash: 16062ce768787384c81fe17a7a60c7e3.
+...repeat many times...
+Then, find the MD5 hash of that hash: a107ff634856bb300138cac6568c0f24.
+So, the stretched hash for index 0 in this situation is a107ff.... 
+In the end, you find the original hash (one use of MD5), then find the hash-of-the-previous-hash 2016 times, for a total of 2017 uses of MD5.
+
+The rest of the process remains the same, but now the keys are entirely different. 
+Again for salt abc:
+
+The first triple (222, at index 5) has no matching 22222 in the next thousand hashes.
+The second triple (eee, at index 10) hash a matching eeeee at index 89, and so it is the first key.
+Eventually, index 22551 produces the 64th key (triple fff with matching fffff at index 22859.
+Given the actual salt in your puzzle input and using 2016 extra MD5 calls of key stretching, what index now produces your 64th one-time pad key?
+
 */
 
 namespace Day14
@@ -38,15 +72,18 @@ namespace Day14
     class Program
     {
         static string sSalt;
+        static MD5 sMD5;
+        readonly static Dictionary<int, string> sComputedMD5s = new Dictionary<int, string>(500000);
 
         private Program(string inputFile, bool part1)
         {
             var lines = AoC.Program.ReadLines(inputFile);
+            Parse(lines);
             if (part1)
             {
-                var result1 = -666;
+                var result1 = FindIndex(64, 0);
                 Console.WriteLine($"Day14 : Result1 {result1}");
-                var expected = 280;
+                var expected = 16106;
                 if (result1 != expected)
                 {
                     throw new InvalidProgramException($"Part1 is broken {result1} != {expected}");
@@ -54,9 +91,9 @@ namespace Day14
             }
             else
             {
-                var result2 = -123;
+                var result2 = FindIndex(64, 2016);
                 Console.WriteLine($"Day14 : Result2 {result2}");
-                var expected = 1797;
+                var expected = 22423;
                 if (result2 != expected)
                 {
                     throw new InvalidProgramException($"Part2 is broken {result2} != {expected}");
@@ -71,10 +108,120 @@ namespace Day14
                 throw new InvalidProgramException($"Input must be a single line {lines.Length}");
             }
             sSalt = lines[0].Trim();
+            sMD5 = MD5.Create();
+            sComputedMD5s.Clear();
         }
 
-        public static int FindIndex(int count)
+        static string GetHashASCIIBytes(int index, int stretchingCount)
         {
+            if (sComputedMD5s.TryGetValue(index, out string hash))
+            {
+                return hash;
+            }
+            var totalKey = $"{sSalt}{index}";
+            byte[] inputBytes = Encoding.ASCII.GetBytes(totalKey);
+            byte[] asciiBytes = new byte[32];
+            for (var i = 0; i <= stretchingCount; ++i)
+            {
+                byte[] hashBytes = sMD5.ComputeHash(inputBytes);
+                for (var j = 0; j < hashBytes.Length; ++j)
+                {
+                    var sourceByte = hashBytes[j];
+                    byte c1 = (byte)((sourceByte >> 4) & 0xF);
+                    byte c2 = (byte)((sourceByte >> 0) & 0xF);
+                    if (c1 > 9)
+                    {
+                        c1 += (byte)'a' - 10;
+                    }
+                    else
+                    {
+                        c1 += (byte)'0';
+                    }
+                    if (c2 > 9)
+                    {
+                        c2 += (byte)'a' - 10;
+                    }
+                    else
+                    {
+                        c2 += (byte)'0';
+                    }
+                    asciiBytes[j * 2 + 0] = c1;
+                    asciiBytes[j * 2 + 1] = c2;
+                }
+                inputBytes = asciiBytes;
+            }
+            string hashString = "";
+            for (var i = 0; i < asciiBytes.Length; ++i)
+            {
+                hashString += (char)asciiBytes[i];
+            }
+            sComputedMD5s[index] = hashString;
+            return hashString;
+        }
+
+        static (int, char) FindThreeCharHash(int startIndex, int stretchingCount)
+        {
+            for (var index = startIndex; index < startIndex + 1000; ++index)
+            {
+                var asciiBytes = GetHashASCIIBytes(index, stretchingCount);
+                for (var i = 0; i < asciiBytes.Length - 2; ++i)
+                {
+                    var c = asciiBytes[i];
+                    if ((c == asciiBytes[i + 1]) && (c == asciiBytes[i + 2]))
+                    {
+                        return (index, c);
+                    }
+                }
+            }
+            throw new InvalidProgramException($"Could not find 3 character hash starting from {startIndex}");
+        }
+
+        static bool FindFiveCharHash(int startIndex, char c, int stretchingCount)
+        {
+            for (var index = startIndex; index <= startIndex + 1000; ++index)
+            {
+                var asciiBytes = GetHashASCIIBytes(index, stretchingCount);
+                for (var i = 0; i < asciiBytes.Length - 4; ++i)
+                {
+                    bool foundMatch = true;
+                    for (var j = 0; j < 5; ++j)
+                    {
+                        if (asciiBytes[i + j] != c)
+                        {
+                            foundMatch = false;
+                            break;
+                        }
+                    }
+                    if (foundMatch)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static int FindIndex(int targetCount, int stretchingCount)
+        {
+            var foundCount = 0;
+            var index = 0;
+            while (index < 300000)
+            {
+                char c;
+                int threeIndex;
+                (threeIndex, c) = FindThreeCharHash(index, stretchingCount);
+                //Console.WriteLine($"Found Three Char Start {threeIndex} {c:x}");
+                if (FindFiveCharHash(threeIndex + 1, c, stretchingCount))
+                {
+                    //Console.WriteLine($"{foundCount} Found valid key {threeIndex}");
+                    ++foundCount;
+                    if (foundCount == targetCount)
+                    {
+                        return threeIndex;
+                    }
+                }
+                index = threeIndex + 1;
+            }
             return -1;
         }
 
--- AoC2016/Day14/Day14Tests.cs
+++ AoC2016/Day14/Day14Tests.cs
@@ -6,11 +6,15 @@ namespace Day14
     public class Tests
     {
         [Test]
-        [TestCase("abc", 22768)]
-        public void Day14(string salt, int expectedIndex)
+        [TestCase("abc", 1, 0, 39)]
+        [TestCase("abc", 2, 0, 92)]
+        [TestCase("abc", 64, 0, 22728)]
+        [TestCase("abc", 1, 2016, 10)]
+        [TestCase("abc", 64, 2016, 22551)]
+        public void Day14(string salt, int keyCount, int stretchingCount, int expectedIndex)
         {
             Program.Parse(new string[] { salt });
-            Assert.That(Program.FindIndex(64), Is.EqualTo(expectedIndex));
+            Assert.That(Program.FindIndex(keyCount, stretchingCount), Is.EqualTo(expectedIndex));
         }
     }
 }
