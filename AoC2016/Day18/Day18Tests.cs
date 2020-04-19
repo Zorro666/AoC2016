03a51647bf26b0a623b43b801a56e1e1978947e0
Jake Turner <jake@unity3d.com>
2020-04-19 09:13:08 +0100

    Day18 Part1 ready to start

--- AoC2016/Day18/Day18Tests.cs
+++ AoC2016/Day18/Day18Tests.cs
@@ -5,11 +5,30 @@ namespace Day18
     [TestFixture]
     public class Tests
     {
+
         [Test]
-        [TestCase("", -3)]
-        public void Day18(string directions, int expected)
+        [TestCase("..^^.", 1, ".^^^^", TestName = "Small Row 1")]
+        [TestCase("..^^.", 2, "^^..^", TestName = "Small Row 2")]
+        [TestCase(".^^.^.^^^^", 1, "^^^...^..^", TestName = "Large Row 1")]
+        [TestCase(".^^.^.^^^^", 2, "^.^^.^.^^.", TestName = "Large Row 2")]
+        [TestCase(".^^.^.^^^^", 3, "..^^...^^^", TestName = "Large Row 3")]
+        [TestCase(".^^.^.^^^^", 4, ".^^^^.^^.^", TestName = "Large Row 4")]
+        [TestCase(".^^.^.^^^^", 5, "^^..^.^^..", TestName = "Large Row 5")]
+        [TestCase(".^^.^.^^^^", 6, "^^^^..^^^.", TestName = "Large Row 6")]
+        [TestCase(".^^.^.^^^^", 7, "^..^^^^.^^", TestName = "Large Row 7")]
+        [TestCase(".^^.^.^^^^", 8, ".^^^..^.^^", TestName = "Large Row 8")]
+        [TestCase(".^^.^.^^^^", 9, "^^.^^^..^^", TestName = "Large Row 9")]
+        public void ComputeRow(string start, int row, string expectedRow)
         {
-            Assert.Fail();
+            Assert.That(Program.ComputeRow(start, row), Is.EqualTo(expectedRow));
+        }
+
+        [Test]
+        [TestCase("..^^.", 3, 6, TestName = "Small Count 6")]
+        [TestCase(".^^.^.^^^^", 10, 38, TestName = "Large Count 38")]
+        public void CountSafeTiles(string start, int rowCount, string expectedSafeTiles)
+        {
+            Assert.That(Program.CountSafeTiles(start, rowCount), Is.EqualTo(expectedSafeTiles));
         }
     }
 }
