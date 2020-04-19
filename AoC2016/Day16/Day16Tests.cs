37f6fd7f6c0c783018c635664b2a385c49987309
Jake Turner <jake@unity3d.com>
2020-04-19 08:41:43 +0100

    Day16 Part1 & Part2 done

--- AoC2016/Day16/Day16Tests.cs
+++ AoC2016/Day16/Day16Tests.cs
@@ -6,10 +6,27 @@ namespace Day16
     public class Tests
     {
         [Test]
-        [TestCase("", -3)]
-        public void Day16(string directions, int expected)
+        [TestCase("1", "100")]
+        [TestCase("0", "001")]
+        [TestCase("11111", "11111000000")]
+        [TestCase("111100001010", "1111000010100101011110000")]
+        public void DragonCurve(string input, string expected)
         {
-            Assert.Fail();
+            Assert.That(Program.DragonCurve(input), Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase("110010110100", "100")]
+        public void Checksum(string input, string expected)
+        {
+            Assert.That(Program.Checksum(input), Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase("10000", 20, "01100")]
+        public void FillDisk(string start, int diskSize, string expected)
+        {
+            Assert.That(Program.FillDisk(start, diskSize), Is.EqualTo(expected));
         }
     }
 }
