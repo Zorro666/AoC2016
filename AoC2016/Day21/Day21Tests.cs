fa8b78891214f0361d7b258e09e5d1e0122ba308
Jake Turner <jake@unity3d.com>
2020-04-24 14:42:07 +0100

    Day21 Start

--- AoC2016/Day21/Day21Tests.cs
+++ AoC2016/Day21/Day21Tests.cs
@@ -6,10 +6,17 @@ namespace Day21
     public class Tests
     {
         [Test]
-        [TestCase("", -3)]
-        public void Day21(string directions, int expected)
+        [TestCase("swap position 4 with position 0", "abcde", "ebcda")]
+        [TestCase("swap letter d with letter b", "ebcda", "edcba")]
+        [TestCase("reverse positions 0 through 4", "edcba", "abcde")]
+        [TestCase("rotate left 1 step", "abcde", "bcdea")]
+        [TestCase("move position 1 to position 4", "bcdea", "bdeac")]
+        [TestCase("move position 3 to position 0", "bdeac", "abdec")]
+        [TestCase("rotate based on position of letter b", "abdec", "ecabd")]
+        [TestCase("rotate based on position of letter d", "ecabd", "decab")]
+        public void ScrambleOperation(string operation, string input, string expectedResult)
         {
-            Assert.Fail();
+            Assert.That(Program.Operation(operation, input), Is.EqualTo(expectedResult));
         }
     }
 }
