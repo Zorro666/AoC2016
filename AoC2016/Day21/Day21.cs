92fc37a14927b9bf0223dbb09d334d37a8468199
Jake Turner <jake@unity3d.com>
2020-04-25 10:18:55 +0100

    Day21 Part2 WIP

--- AoC2016/Day21/Day21.cs
+++ AoC2016/Day21/Day21.cs
@@ -46,13 +46,29 @@ namespace Day21
 {
     class Program
     {
+        struct Operation
+        {
+            public enum Command { SWAP, ROTATE, MOVE, REVERSE };
+            public Command command;
+            public int indexA;
+            public int indexB;
+            public char letterA;
+            public char letterB;
+            public int rotateCount;
+        }
+
+        static Operation[] sOperations;
+
         private Program(string inputFile, bool part1)
         {
             var lines = AoC.Program.ReadLines(inputFile);
+            Parse(lines);
+            var start = "abcdefgh";
+            var scramble = Scramble(start);
 
             if (part1)
             {
-                var result1 = Process("abcdefgh", lines);
+                var result1 = scramble;
                 Console.WriteLine($"Day21 : Result1 {result1}");
                 var expected = "dgfaehcb";
                 if (result1 != expected)
@@ -62,9 +78,15 @@ namespace Day21
             }
             else
             {
-                var result2 = -123;
+                var scramble2 = "fbgdceah";
+                var result2 = UnScramble(start, scramble, scramble2);
+                var checkScramble2 = Scramble(result2);
+                if (checkScramble2 != scramble2)
+                {
+                    throw new InvalidProgramException($"Part2 is broken {checkScramble2} != {scramble2}");
+                }
                 Console.WriteLine($"Day21 : Result2 {result2}");
-                var expected = 1797;
+                var expected = "dhafcgbe";
                 if (result2 != expected)
                 {
                     throw new InvalidProgramException($"Part2 is broken {result2} != {expected}");
@@ -72,13 +94,48 @@ namespace Day21
             }
         }
 
-        static string Process(string start, string[] lines)
+        public static string UnScramble(string inputA, string scrambleA, string scrambledInput)
+        {
+            if (inputA.Length != scrambleA.Length)
+            {
+                throw new InvalidProgramException($"UnScramble inputA and scrambleA Lengths do not match {inputA.Length} != {scrambleA.Length}");
+            }
+            if (scrambledInput.Length != scrambleA.Length)
+            {
+                throw new InvalidProgramException($"UnScramble scrambledInput and scrambleA Lengths do not match {scrambledInput.Length} != {scrambleA.Length}");
+            }
+
+            int[] inputToOutputIndexes = new int[inputA.Length];
+            for (var inputIndex = 0; inputIndex < inputA.Length; ++inputIndex)
+            {
+                var inputLetter = inputA[inputIndex];
+                inputToOutputIndexes[inputIndex] = scrambleA.IndexOf(inputLetter);
+            }
+            var unscrambledChars = scrambledInput.ToCharArray();
+            for (var inputIndex = 0; inputIndex < inputA.Length; ++inputIndex)
+            {
+                var outputIndex = inputToOutputIndexes[inputIndex];
+                unscrambledChars[inputIndex] = scrambledInput[outputIndex];
+            }
+            return new string(unscrambledChars);
+        }
+
+        public static void Parse(string[] lines)
+        {
+            sOperations = new Operation[lines.Length];
+            for (var o = 0; o < sOperations.Length; o++)
+            {
+                sOperations[o] = ParseOperation(lines[o]);
+            }
+        }
+
+        public static string Scramble(string start)
         {
             var input = start;
             string output = null;
-            foreach (var line in lines)
+            foreach (var operation in sOperations)
             {
-                output = Operation(line, input);
+                output = ProcessOperation(operation, input);
                 input = output;
             }
             return output;
@@ -104,94 +161,36 @@ namespace Day21
             return index;
         }
 
-        public static string Operation(string operation, string input)
+        static string ProcessOperation(Operation operation, string input)
         {
-            var tokens = operation.Split(' ');
-            var cmd = tokens[0];
             var inputChars = input.ToCharArray();
             var outputChars = input.ToCharArray();
-            if (cmd == "swap")
+
+            var cmd = operation.command;
+            if (cmd == Operation.Command.SWAP)
             {
-                //"swap position X with position Y" means that the letters at indexes X and Y (counting from 0) should be swapped.
-                //"swap letter X with letter Y "means that the letters X and Y should be swapped (regardless of where they appear in the string).
-                var option = tokens[1];
-                if ((option != "position") && (option != "letter"))
+                var indexA = operation.indexA;
+                var indexB = operation.indexB;
+                if ((indexA < 0) && (indexB < 0))
                 {
-                    throw new InvalidProgramException($"Expected `position` or 'letter' got {tokens[1]} operation:{operation}");
-                }
-                if (tokens[3] != "with")
-                {
-                    throw new InvalidProgramException($"Expected `with` got {tokens[3]} operation:{operation}");
-                }
-                if (tokens[4] != option)
-                {
-                    throw new InvalidProgramException($"Expected `{option}` got {tokens[4]} operation:{operation}");
-                }
-                int indexA;
-                int indexB;
-                if (option == "position")
-                {
-                    indexA = ParseIndex(tokens[2]);
-                    indexB = ParseIndex(tokens[5]);
-                }
-                else
-                {
-                    var letterA = ParseLetter(tokens[2]);
-                    var letterB = ParseLetter(tokens[5]);
-                    indexA = input.IndexOf(letterA);
-                    indexB = input.IndexOf(letterB);
+                    indexA = input.IndexOf(operation.letterA);
+                    indexB = input.IndexOf(operation.letterB);
                 }
                 if ((indexA < 0) || (indexA >= input.Length))
                 {
-                    throw new InvalidProgramException($"Invalid index {indexA} {indexB} range:0-{input.Length} operation:{operation}");
+                    throw new InvalidProgramException($"Invalid index {indexA} {indexB} range:0-{input.Length}");
                 }
                 var charA = inputChars[indexA];
                 var charB = inputChars[indexB];
                 outputChars[indexB] = charA;
                 outputChars[indexA] = charB;
             }
-            else if (cmd == "rotate")
+            else if (cmd == Operation.Command.ROTATE)
             {
-                //"rotate left/right X steps "means that the whole string should be rotated; for example, one right rotation would turn abcd into dabc.
-                //"rotate based on position of letter X "means that the whole string should be rotated to the right based on the index of letter X (counting from 0) as determined before this instruction does any rotations. 
-                //Once the index is determined, rotate the string to the right one time, plus a number of times equal to that index, plus one additional time if the index was at least 4.
-                var option = tokens[1];
-                if ((option != "left") && (option != "right") && (option != "based"))
+                var count = operation.rotateCount;
+                var letter = operation.letterA;
+                if ((count == -1) && (letter != '0'))
                 {
-                    throw new InvalidProgramException($"Expected `left` or 'right' or 'based' got {tokens[1]} operation:{operation}");
-                }
-                int count = -1;
-                if ((option == "left") || (option == "right"))
-                {
-                    if ((tokens[3] != "steps") && (tokens[3] != "step"))
-                    {
-                        throw new InvalidProgramException($"Expected `steps` or `step` got {tokens[3]} operation:{operation}");
-                    }
-                    count = int.Parse(tokens[2]);
-                    if (option == "left")
-                    {
-                        count = -count;
-                    }
-                }
-                else
-                {
-                    if (tokens[2] != "on")
-                    {
-                        throw new InvalidProgramException($"Expected `on` got {tokens[2]} operation:{operation}");
-                    }
-                    if (tokens[3] != "position")
-                    {
-                        throw new InvalidProgramException($"Expected `position` got {tokens[3]} operation:{operation}");
-                    }
-                    if (tokens[4] != "of")
-                    {
-                        throw new InvalidProgramException($"Expected `of` got {tokens[4]} operation:{operation}");
-                    }
-                    if (tokens[5] != "letter")
-                    {
-                        throw new InvalidProgramException($"Expected `letter` got {tokens[5]} operation:{operation}");
-                    }
-                    var letter = ParseLetter(tokens[6]);
                     var index = input.IndexOf(letter);
                     count = 1 + index;
                     if (index >= 4)
@@ -211,19 +210,10 @@ namespace Day21
                     outputChars[outputIndex] = inputChar;
                 }
             }
-            else if (cmd == "reverse")
+            else if (cmd == Operation.Command.REVERSE)
             {
-                //"reverse positions X through Y" means that the span of letters at indexes X through Y (including the letters at X and Y) should be reversed in order.
-                if (tokens[1] != "positions")
-                {
-                    throw new InvalidProgramException($"Expected `positions` got {tokens[1]} operation:{operation}");
-                }
-                if (tokens[3] != "through")
-                {
-                    throw new InvalidProgramException($"Expected `through` got {tokens[3]} operation:{operation}");
-                }
-                var indexA = ParseIndex(tokens[2]);
-                var indexB = ParseIndex(tokens[4]);
+                var indexA = operation.indexA;
+                var indexB = operation.indexB;
                 for (var i = indexA; i <= indexB; ++i)
                 {
                     var inputChar = inputChars[i];
@@ -231,23 +221,10 @@ namespace Day21
                     outputChars[outputIndex] = inputChar;
                 }
             }
-            else if (cmd == "move")
+            else if (cmd == Operation.Command.MOVE)
             {
-                //"move position X to position Y" means that the letter which is at index X should be removed from the string, then inserted such that it ends up at index Y.
-                if (tokens[1] != "position")
-                {
-                    throw new InvalidProgramException($"Expected `position` got {tokens[1]} operation:{operation}");
-                }
-                if (tokens[3] != "to")
-                {
-                    throw new InvalidProgramException($"Expected `to` got {tokens[3]} operation:{operation}");
-                }
-                if (tokens[4] != "position")
-                {
-                    throw new InvalidProgramException($"Expected `position` got {tokens[4]} operation:{operation}");
-                }
-                var indexA = ParseIndex(tokens[2]);
-                var indexB = ParseIndex(tokens[5]);
+                var indexA = operation.indexA;
+                var indexB = operation.indexB;
                 var movedChar = inputChars[indexA];
                 // Remove from source
                 for (var i = indexA; i < inputChars.Length - 1; ++i)
@@ -270,6 +247,134 @@ namespace Day21
                 throw new InvalidProgramException($"Unknown command {cmd} operation:{operation}");
             }
             return new string(outputChars);
+        }
+
+        static Operation ParseOperation(string operation)
+        {
+            var tokens = operation.Split(' ');
+            var cmd = tokens[0];
+
+            Operation op;
+            op.indexA = -1;
+            op.indexB = -1;
+            op.rotateCount = -1;
+            op.letterA = '0';
+            op.letterB = '0';
+
+            if (cmd == "swap")
+            {
+                op.command = Operation.Command.SWAP;
+                //"swap position X with position Y" means that the letters at indexes X and Y (counting from 0) should be swapped.
+                //"swap letter X with letter Y "means that the letters X and Y should be swapped (regardless of where they appear in the string).
+                var option = tokens[1];
+                if ((option != "position") && (option != "letter"))
+                {
+                    throw new InvalidProgramException($"Expected `position` or 'letter' got {tokens[1]} operation:{operation}");
+                }
+                if (tokens[3] != "with")
+                {
+                    throw new InvalidProgramException($"Expected `with` got {tokens[3]} operation:{operation}");
+                }
+                if (tokens[4] != option)
+                {
+                    throw new InvalidProgramException($"Expected `{option}` got {tokens[4]} operation:{operation}");
+                }
+                if (option == "position")
+                {
+                    op.indexA = ParseIndex(tokens[2]);
+                    op.indexB = ParseIndex(tokens[5]);
+                }
+                else
+                {
+                    op.letterA = ParseLetter(tokens[2]);
+                    op.letterB = ParseLetter(tokens[5]);
+                }
+            }
+            else if (cmd == "rotate")
+            {
+                op.command = Operation.Command.ROTATE;
+                //"rotate left/right X steps "means that the whole string should be rotated; for example, one right rotation would turn abcd into dabc.
+                //"rotate based on position of letter X "means that the whole string should be rotated to the right based on the index of letter X (counting from 0) as determined before this instruction does any rotations. 
+                //Once the index is determined, rotate the string to the right one time, plus a number of times equal to that index, plus one additional time if the index was at least 4.
+                var option = tokens[1];
+                if ((option != "left") && (option != "right") && (option != "based"))
+                {
+                    throw new InvalidProgramException($"Expected `left` or 'right' or 'based' got {tokens[1]} operation:{operation}");
+                }
+                int count = -1;
+                if ((option == "left") || (option == "right"))
+                {
+                    if ((tokens[3] != "steps") && (tokens[3] != "step"))
+                    {
+                        throw new InvalidProgramException($"Expected `steps` or `step` got {tokens[3]} operation:{operation}");
+                    }
+                    count = int.Parse(tokens[2]);
+                    if (option == "left")
+                    {
+                        count = -count;
+                    }
+                    op.rotateCount = count;
+                }
+                else
+                {
+                    if (tokens[2] != "on")
+                    {
+                        throw new InvalidProgramException($"Expected `on` got {tokens[2]} operation:{operation}");
+                    }
+                    if (tokens[3] != "position")
+                    {
+                        throw new InvalidProgramException($"Expected `position` got {tokens[3]} operation:{operation}");
+                    }
+                    if (tokens[4] != "of")
+                    {
+                        throw new InvalidProgramException($"Expected `of` got {tokens[4]} operation:{operation}");
+                    }
+                    if (tokens[5] != "letter")
+                    {
+                        throw new InvalidProgramException($"Expected `letter` got {tokens[5]} operation:{operation}");
+                    }
+                    op.letterA = ParseLetter(tokens[6]);
+                }
+            }
+            else if (cmd == "reverse")
+            {
+                op.command = Operation.Command.REVERSE;
+                //"reverse positions X through Y" means that the span of letters at indexes X through Y (including the letters at X and Y) should be reversed in order.
+                if (tokens[1] != "positions")
+                {
+                    throw new InvalidProgramException($"Expected `positions` got {tokens[1]} operation:{operation}");
+                }
+                if (tokens[3] != "through")
+                {
+                    throw new InvalidProgramException($"Expected `through` got {tokens[3]} operation:{operation}");
+                }
+                op.indexA = ParseIndex(tokens[2]);
+                op.indexB = ParseIndex(tokens[4]);
+            }
+            else if (cmd == "move")
+            {
+                op.command = Operation.Command.MOVE;
+                //"move position X to position Y" means that the letter which is at index X should be removed from the string, then inserted such that it ends up at index Y.
+                if (tokens[1] != "position")
+                {
+                    throw new InvalidProgramException($"Expected `position` got {tokens[1]} operation:{operation}");
+                }
+                if (tokens[3] != "to")
+                {
+                    throw new InvalidProgramException($"Expected `to` got {tokens[3]} operation:{operation}");
+                }
+                if (tokens[4] != "position")
+                {
+                    throw new InvalidProgramException($"Expected `position` got {tokens[4]} operation:{operation}");
+                }
+                op.indexA = ParseIndex(tokens[2]);
+                op.indexB = ParseIndex(tokens[5]);
+            }
+            else
+            {
+                throw new InvalidProgramException($"Unknown command {cmd} operation:{operation}");
+            }
+            return op;
         }
 
         public static void Run()
--- AoC2016/Day21/Day21Tests.cs
+++ AoC2016/Day21/Day21Tests.cs
@@ -15,9 +15,27 @@ namespace Day21
         [TestCase("move position 3 to position 0", "bdeac", "abdec")]
         [TestCase("rotate based on position of letter b", "abdec", "ecabd")]
         [TestCase("rotate based on position of letter d", "ecabd", "decab")]
-        public void ScrambleOperation(string operation, string input, string expectedResult)
+        public void Scramble(string operation, string input, string expectedResult)
         {
-            Assert.That(Program.Operation(operation, input), Is.EqualTo(expectedResult));
+            Program.Parse(new string[] { operation });
+            Assert.That(Program.Scramble(input), Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase("swap position 4 with position 0", "abcde")]
+        [TestCase("swap letter d with letter b", "ebcda")]
+        [TestCase("reverse positions 0 through 4", "edcba")]
+        [TestCase("rotate left 1 step", "abcde")]
+        [TestCase("rotate right 1 step", "abcde")]
+        [TestCase("move position 1 to position 4", "bcdea")]
+        [TestCase("move position 3 to position 0", "bdeac")]
+        [TestCase("rotate based on position of letter b", "abdec")]
+        [TestCase("rotate based on position of letter d", "ecabd")]
+        public void UnScrambleOperation(string operation, string input)
+        {
+            Program.Parse(new string[] { operation });
+            var scramble = Program.Scramble(input);
+            Assert.That(Program.UnScramble(input, scramble, scramble), Is.EqualTo(input));
         }
     }
 }
